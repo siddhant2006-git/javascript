5db5335d83540525408ed082976ea54993b0a652
siddhant-bhatnagar <siddhantbhatnagar11@gmail.com>
2026-04-29 23:43:52 +0530

    weather forcasting

--- weather_forcasting/index.js
+++ weather_forcasting/index.js
@@ -1,83 +1,25 @@
-const fileInput = document.getElementById("fileInput");
-const preview = document.getElementById("preview");
-const fileInfo = document.getElementById("fileInfo");
-const uploadBtn = document.getElementById("uploadBtn");
-const progressBar = document.getElementById("progressBar");
-const statusText = document.getElementById("status");
-
-let selectedFile = null;
-
-// Validate type
-function validateType(file) {
-  const types = ["image/jpeg", "image/png", "image/webp"];
-  return types.includes(file.type);
-}
-
-// Validate size
-function validateSize(file) {
-  return file.size <= 2 * 1024 * 1024;
-}
-
-// Show preview
-function showPreview(file) {
-  const url = URL.createObjectURL(file);
-  preview.src = url;
-  preview.style.display = "block";
-
-  preview.onload = () => URL.revokeObjectURL(url);
-}
-
-// Show file info
-function showInfo(file) {
-  const size = (file.size / 1024).toFixed(2);
-  fileInfo.innerText = `${file.name} - ${size} KB`;
-}
-
-// Handle file select
-fileInput.addEventListener("change", () => {
-  const file = fileInput.files[0];
-  if (!file) return;
-
-  if (!validateType(file)) {
-    alert("Only images allowed");
-    return;
-  }
-
-  if (!validateSize(file)) {
-    alert("Max size 2MB");
-    return;
-  }
-
-  selectedFile = file;
-  showPreview(file);
-  showInfo(file);
-});
-
-// Upload file
-uploadBtn.addEventListener("click", () => {
-  if (!selectedFile) return alert("Select file first");
-
-  const formData = new FormData();
-  formData.append("file", selectedFile);
-
-  const xhr = new XMLHttpRequest();
-  xhr.open("POST", "http://localhost:3000/upload");
-
-  // Progress
-  xhr.upload.onprogress = (e) => {
-    const percent = (e.loaded / e.total) * 100;
-    progressBar.style.width = percent + "%";
+async function climate() {
+  const url =
+    "https://weather338.p.rapidapi.com/locations/get-details?placeid=69bedc6a5b6e977993fb3e5344e3c06d8bc36a1fb6754c3ddfb5310a3c6d6c87&language=en-US";
+  const options = {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": "3b9ba73c5amshb36782d4fb8f2aep19f9d3jsnf7be6ebec27d",
+      "x-rapidapi-host": "weather338.p.rapidapi.com",
+      "Content-Type": "application/json",
+    },
   };
 
-  xhr.onload = () => {
-    if (xhr.status === 200) {
-      const res = JSON.parse(xhr.responseText);
-      statusText.innerText = "Upload Success";
-      console.log(res.filePath);
-    } else {
-      statusText.innerText = "Upload Failed";
-    }
-  };
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
 
-  xhr.send(formData);
-});
+  
+}
+
+
+
