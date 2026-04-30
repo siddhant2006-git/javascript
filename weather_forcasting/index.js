06d5b86b8c1f6b3826ac4fc301d96e8e6188eefe
siddhant-bhatnagar <siddhantbhatnagar11@gmail.com>
2026-04-30 20:02:50 +0530

    weather app

--- weather_forcasting/index.js
+++ weather_forcasting/index.js
@@ -1,3 +1,9 @@
+const tempBtn = document.getElementById("temparature");
+const input = document.getElementById("city");
+const output = document.getElementById("output");
+
+tempBtn.addEventListener("click", climate);
+
 async function climate() {
   const url =
     "https://weather338.p.rapidapi.com/locations/get-details?placeid=69bedc6a5b6e977993fb3e5344e3c06d8bc36a1fb6754c3ddfb5310a3c6d6c87&language=en-US";
@@ -11,15 +17,22 @@ async function climate() {
   };
 
   try {
+    output.innerHTML = "";
     const response = await fetch(url, options);
     const result = await response.json();
     console.log(result);
+
+    const wheather = document.createElement("div");
+
+    wheather.innerHTML = `
+  <h2>${result.location.city}</h2>
+  <p>Latitude: ${result.location.latitude}</p>
+  <p>Longitude: ${result.location.longitude}</p>
+  <p>Country: ${result.location.country}</p>
+`;
+
+    output.appendChild(wheather);
   } catch (error) {
     console.error(error);
   }
-
-  
 }
-
-
-
