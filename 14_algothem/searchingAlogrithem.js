93d750798a4f398fc538359ca6885706704f790d
siddhant-2006 <siddhantbhatnagar11@gmail.com>
2026-05-17 23:42:38 +0530

    Refactor searching algorithms and add quick sort implementation

--- 14_algothem/searchingAlogrithem.js
+++ 14_algothem/searchingAlogrithem.js
@@ -9,7 +9,7 @@ function linearsearching(arr, target) {
   return -1; // not found
 }
 
-  const number = [10, 20, 30, ,45,40, 50];
+const number = [10, 20, 30, , 45, 40, 50];
 console.log(linearsearching(number, 45));
 
 // binary searching
@@ -30,9 +30,33 @@ function binarySearch(arr, target) {
       end = mid - 1;
     }
   }
-  return -1; // not found 
+  return -1; // not found
 }
 const numbers = [10, 20, 30, 40, 50, 60, 70];
 console.log(binarySearch(numbers, 40));
 
+// quick shorting
 
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  let pivot = arr[arr.length - 1];
+  let left = [];
+  let right = [];
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+const arr = [10, 20, 374, 373, 474, 335, 33];
+
+console.log(quickSort(arr));
